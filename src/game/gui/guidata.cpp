987efb55aaa29fd49597f856249f4c6e2b854e0b
Mike McLean <Libolt@libolt.net>
2016-05-31 12:45:20 -0700

    Still debugging crashes in the court code

--- src/game/gui/guidata.cpp
+++ src/game/gui/guidata.cpp
@@ -67,11 +67,15 @@ bool GUISystem::addCourtSelectionMenuData()  // adds data to Player Start Select
     {
         courtName.push_back(courtInstance[x].getName());
     }
-    logMsg("courtInstane.size == " +convert->toString(courtInstance.size()));
-    exit(0);
+    if (courtInstance.size() == 0)
+    {
+        logMsg("courtInstane.size == " +convert->toString(courtInstance.size()));
+//        exit(0);
+    }
+    
     logMsg("courtName = " +courtName[0]);
     logMsg("courtName size = " +convert->toString(courtName.size()));
-    exit(0);
+//    exit(0);
     size_t i = 0;
     while (i<courtName.size())
     {

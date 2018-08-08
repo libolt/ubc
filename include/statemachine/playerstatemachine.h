a26c643976016154e6459e2e85adca8fecd42bf3
Mike McLean <libolt@libolt.net>
2018-08-08 14:47:47 -0700

    Revert "Began revamping stateMachine to use sharedPtr"
    This reverts commit fc953ed7e56055ec04df086a1d0bb3112e7ba660.

--- include/statemachine/playerstatemachine.h
+++ include/statemachine/playerstatemachine.h
@@ -43,13 +43,13 @@ public:
     playerStateMachine();
 
     // External events taken by this state machine
-    void setPNode(sharedPtr<playerSMData> data);  // sets the node to that of the entity parent object
-    void setPModel(sharedPtr<playerSMData> data);  // sets the model to that of the entity parent object
-    void setSpeed(sharedPtr<playerSMData> data);
+    void setPNode(playerSMData *data);  // sets the node to that of the entity parent object
+    void setPModel(playerSMData *data);  // sets the model to that of the entity parent object
+    void setSpeed(playerSMData *data);
     void halt();
-    void pJump(sharedPtr<playerSMData> data);
-    void pChangeDirection(sharedPtr<playerSMData> data);
-    void pChangePosition(sharedPtr<playerSMData> data);
+    void pJump(playerSMData *data);
+    void pChangeDirection(playerSMData *data);
+    void pChangePosition(playerSMData *data);
 
     
 private:
